14206c8e39cdfe2f86389802fa61e615b30d0e93
the-vampiire <vampiirecodes@gmail.com>
2020-07-20 18:38:52 -0500

    studio pwsh script: bash runcommand solution

--- src/chapters/powershell-intro/instructor/az-cli-runcommand.sh
+++ src/chapters/powershell-intro/instructor/az-cli-runcommand.sh
@@ -2,11 +2,16 @@
 
 vm_name=$(az vm show --query "name" -o tsv)
 
+
+# az vm run-command invoke --command-id RunShellScript -n "$vm_name" --scripts "@configure-vm.sh" --query "value[0].message"
+# az vm run-command invoke --command-id RunShellScript -n "$vm_name" --scripts "@configure-ssl.sh" --query "value[0].message" 
+# az vm run-command invoke --command-id RunShellScript -n "$vm_name" --scripts @deliver-deploy.sh --query "value[0].message"
+
 scripts=(configure-vm.sh configure-ssl.sh deliver-deploy.sh)
 
 for script in "${scripts[@]}"; do
    output_file="/tmp/${script}.out"
    echo "writing output to $output_file"
 
-   az vm run-command invoke --command-id RunShellScript -n "$vm_name" --scripts "@$script" --query "value.message" -o tsv > $output_file
+   az vm run-command invoke --command-id RunShellScript -n "$vm_name" --scripts "@$script" --query "value[0].message" -o tsv > "$output_file"
 done
